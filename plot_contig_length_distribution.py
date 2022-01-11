b7bfbfff7d8728375caa36d3e0eb1fdfb652359d
snail123815 <durand.dc@hotmail.com>
2022-01-11 08:57:00 +0100

    add cumulative plot

--- plot_contig_length_distribution.py
+++ plot_contig_length_distribution.py
@@ -14,18 +14,39 @@ title = os.path.basename(fa)
 seqs = list(SeqIO.parse(fa,'fasta'))
 seqs.sort(key=lambda x: len(x), reverse=True)
 seqIds = [s.id for s in seqs]
-plt.plot([len(s) for s in seqs])
-plt.title(title)
+
+fig, ax = plt.subplots(1,1)
+ax.plot([len(s) for s in seqs])
+ax.set_title(title)
+
+cumax = ax.twinx()
+cumList = []
+cum = 0
+for l in [len(s) for s in seqs]:
+    cum += l
+    cumList.append(cum)
+cumax.plot(cumList)
+
+n50 = [l for l in cumList if l >= cumList[-1]/2][0]
+n50i = cumList.index(n50)
+n50seq = seqs[n50i]
+
+cumax.scatter(n50i, n50)
+cumax.text(n50i, n50, f' N50=\n {n50seq.id}:{len(n50seq):,}', va='center', ha='left')
+
+ax.text(0.98, 0.92, f'Total {cumList[-1]:,}',
+        transform=ax.transAxes,
+        va='top', ha='right')
+
 i = 0
 while i < 15:
     try:
         t = f"{seqIds[i]}: {len(seqs[i]):.2e}"
-        plt.text(0.98, 0.98-0.05*i, t,
-                 transform=plt.gca().transAxes,
-                 va='top', ha='right'
-                )
+        ax.text(0.98, 0.82-0.05*i, t,
+                transform=ax.transAxes,
+                va='top', ha='right')
     except:
-        pass
+        break
     i += 1
 
 
