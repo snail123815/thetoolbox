82893333329ecb970de323306060a02346f98a58
Chao <c.du@biology.leidenuniv.nl>
2022-11-03 16:52:34 +0100

    proposed consensus sequence call process written, not tested

--- pyBioinfo/consensus_sequence_generator.py
+++ pyBioinfo/consensus_sequence_generator.py
@@ -1,5 +1,33 @@
 from pathlib import Path
 import argparse
+from Bio import SeqIO
 
 
-from pyBioinfo_modules.bio_sequences.vcf_parser import vcfParser
+from pyBioinfo_modules.bio_sequences.vcf_parser import (
+    vcfParser, applyVariancesOnSeqRecords
+)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--vcf', type=Path)
+    parser.add_argument('--genome', type=Path, help='genbank format')
+    parser.add_argument('--output', type=Path)
+
+    args = parser.parse_args()
+
+    varianceDatas = vcfParser(args.vcf)
+
+    seqRecordDict = {
+        rec.id: rec for rec in SeqIO.parse(args.genome, 'genbank')
+    }
+
+    variantSeqRecordDict = applyVariancesOnSeqRecords(
+        varianceDatas, seqRecordDict
+    )
+
+    SeqIO.write(variantSeqRecordDict.values(), args.output)
+
+
+if __name__ == '__main__':
+    main()
