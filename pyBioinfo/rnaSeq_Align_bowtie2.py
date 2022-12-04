c1b8a5dc47c2b81dc7dca3357ca78b58e78df82b
snail123815 <durand.dc@gmail.com>
2022-12-04 19:34:02 +0100

    format and work to line 87

--- pyBioinfo/rnaSeq_Align_bowtie2.py
+++ pyBioinfo/rnaSeq_Align_bowtie2.py
@@ -1,4 +1,5 @@
-# For RNA-Seq alignment, with each file or pair of files per sample. Out put aligned file per sample.
+# For RNA-Seq alignment, with each file or pair of files per sample. Out
+# put aligned file per sample.
 
 
 from subprocess import run
@@ -8,78 +9,84 @@ import os
 import time
 import argparse
 import logging
+from pathlib import Path
 
+env = '~/genvs/shortReads/'
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--raw', required=True, nargs='+', help='path(s) to raw data')
-    parser.add_argument('--out', required=True, help='path to output alignment')
-    parser.add_argument('--genome', nargs="+", required=True, help='path to genome file(s), also supports indexed genome (*.bt2)')
+    parser.add_argument(
+        '--raw',
+        required=True,
+        type=Path,
+        nargs='+',
+        help='path(s) to raw data')
+    parser.add_argument(
+        '--out',
+        required=True,
+        type=Path,
+        help='path to output alignment')
+    parser.add_argument(
+        '--genome',
+        nargs="+",
+        required=True,
+        type=Path,
+        help='path to genome file(s), also supports indexed genome (*.bt2)')
 
-    parser.add_argument('--isPe', action='store_true', help='set if you have pairend')
+    parser.add_argument(
+        '--isPe',
+        action='store_true',
+        help='set if you have pairend')
 
+    # TODO this should be removed
     parser.add_argument('--rawExt', help='extension ".fastq.gz" or ".fq.gz"')
-    parser.add_argument('--pesuffix', nargs=2, help='suffix to pairend file name. eg. a_1_.fq.gz and a_2_.fq.gz, then set this to --pesuffix _1_ _2_')
-    parser.add_argument('--ncpu', type=str, help='number of cpu to use')
-    parser.add_argument('--sampleNames', nargs="+", help='sample names if file names contain random generated string which will unpair the samples after removing pairend suffix')
+    parser.add_argument(
+        '--pesuffix',
+        nargs=2,
+        help='suffix to pairend file name. eg. a_1_.fq.gz and a_2_.fq.gz, then set this to --pesuffix _1_ _2_')
+    parser.add_argument(
+        '--ncpu',
+        default=1,
+        type=int,
+        help='number of cpu to use')
+    parser.add_argument(
+        '--sampleNames',
+        nargs="+",
+        help='sample names if file names contain random generated string which will unpair the samples after removing pairend suffix')
 
     args = parser.parse_args()
 
-    # required arguments
-    rawFolders = args.raw
-    outFolder = args.out
-    genomes = args.genome
+    # TODO check existance of files before creating output dir
+    if not args.out.is_dir():
+        args.out.mkdir(exist_ok=True)
 
-    # boolean
-    isPe = args.isPe
-    peSfx = args.pesuffix
+    toBamNcpu = max(args.ncpu // 8, 1)
 
-    # optional
-    rawExt = args.rawExt
-    samples = args.sampleNames
-    ncpu = args.ncpu
-
-    if isinstance(ncpu, type(None)):
-        ncpu = "1" 
-
-    #print(args)
-    #exit()
-
-    if not os.path.isdir(outFolder):
-        os.makedirs(outFolder)
-    logging.basicConfig(filename=os.path.join(outFolder, 'align.log'), level=logging.DEBUG)
-
-
-    toBamNcpu = str(max(int(int(ncpu)/8), 1))
-
-
+    logging.basicConfig(
+        filename=os.path.join(
+            args.out / 'align.log'),
+        level=logging.DEBUG)
     logging.debug(args)
+    logging.info('=' * 20 + getTime() + '=' * 20)
 
-    logging.info('='*20 + getTime() + '='*20)
+    genomeBowtie2Idx = buildBowtie2idx(
+        [args.genome], out=args.out / 'genomeIdx')
 
-    genomeBowtie2Idx = buildBowtie2idx(genomes, out=os.path.join(outFolder, 'genomeIdx'))
-
-    # Gether files for each sample
-    files = []
-    for folder in rawFolders:
-        for f in os.listdir(folder):
-            # rawExt defined here if not defined
-            if isinstance(rawExt, type(None)):
-                fn, ext = os.path.splitext(f)
-                if ext == '.gz':
-                    ext = os.path.splitext(fn)[1] + ext
-                elif ext in ['.fastq', '.fq']:
-                    pass
-                else:
-                    continue
-                rawExt = ext
-                logging.info(f'Found extension for raw files {rawExt}')
-            if f.endswith(rawExt):
-                files.append(os.path.join(folder,f))
-    assert len(files) > 0, f'Files not found in {rawFolders}'
+    # Gether files for each sample, max depth 2
+    files = [f for f in args.raw.iterdir() if f.is_file()]
+    [files.extend(f for f in d.iterdir() if f.is_file())
+     for d in args.raw.iterdir() if d.is_dir()]
+    for f in files.copy():
+        if f.suffix == '.gz':
+            ext = f.with_suffix('').suffix
+        else:
+            ext = f.suffix
+        if ext not in ['.fastq', '.fq']:
+            files.pop(f)
+    assert len(files) > 0, f'Files not found in {args.raw}'
     files.sort()
-            
+
     # peSfx defined here if not defined
     if isPe:
         peSfx = imputePesuffix(files, rawExt, peSfx)
@@ -93,16 +100,14 @@ def main():
             samples = fns
         samples.sort()
 
-
     sampleFileDict = {}
     for s in samples:
-        fs = [f for f in files if s in f]
-        fs.sort()
+        fs = sorted([f for f in files if s in f])
         if isPe:
             sampleFileDict[s] = {peSfx[0]: [f for f in fs if f[:-len(rawExt)].endswith(peSfx[0])],
                                  peSfx[1]: [f for f in fs if f[:-len(rawExt)].endswith(peSfx[1])]}
         else:
-            sampleFileDict[s] = fs 
+            sampleFileDict[s] = fs
 
     totalTs = time.time()
     assert len(samples) > 0
@@ -139,39 +144,35 @@ def main():
         if result.returncode != 0:
             logging.info('stderr: ' + result.stderr.decode())
             logging.info('stdout: ' + result.stdout.decode())
-        logging.info(result.stderr.decode()) # stderr has logging.info info from bowtie2
+        # stderr has logging.info info from bowtie2
+        logging.info(result.stderr.decode())
         logging.info(f'Finished in {diffTime(ts)}\n')
 
     logging.info(f'All done, time elapsed {diffTime(totalTs)}')
-    logging.info('='*20 + getTime() + '='*20 + '\n'*2)
+    logging.info('=' * 20 + getTime() + '=' * 20 + '\n' * 2)
 
 
-def buildBowtie2idx(fs, out, name=None):
-    if type(fs) == str:
-        fs = [fs]
-    fs = [os.path.realpath(f) for f in fs]
+def buildBowtie2idx(fs: list[Path], out: Path, name=None):
+    fs = [f.resolve() for f in fs]
+    out = out.resolve()
 
     # check if idx file exists
-    genomePath, genomeName = os.path.split(fs[0])
-    genomeName = os.path.splitext(genomeName)[0]
+    genomePath = fs[0].parent
+    genomeName = fs[0].stem
     logging.info(f'genome name: {genomeName}')
 
-    out = os.path.realpath(out)
-    if not os.path.isdir(out):
-        os.makedirs(out)
-    bt2_base = ('_'.join(os.path.splitext(os.path.split(f)[1])[0] for f in fs) if name == None else name)
-    outIdxForUse = os.path.join(out, bt2_base)
+    out.mkdir(exist_ok=True)
+    bt2_base = ('_'.join(f.stem for f in fs) if name is None else name)
+    outIdxForUse = out / bt2_base
 
-    # see if index file already exists
-    outIdxFile0 = outIdxForUse + '.1.bt2'
-    outIdxLargeFile0 = outIdxForUse + '.1.bt21'
-    if os.path.isfile(outIdxFile0):
-        logging.info(f'Found {outIdxFile0}, using it as index.')
+    if any((
+        outIdxForUse.with_suffix('.1.bt2').is_file(),
+        outIdxForUse.with_suffix('.1.bt21').is_file()
+    )):
+        logging.info(
+            f'Found index file {out.glob(bt2_base)}, will not make new ones.')
         return outIdxForUse
-    elif os.path.isfile(outIdxLargeFile0):
-        logging.info(f'Found {outIdxLargeFile0}, using it as index.')
-        return outIdxForUse
-    
+
     # convert gbk to fa
     converted = []
     if os.path.splitext(fs[0])[1] not in ['.fa', '.fasta', '.fna', '.fsa']:
@@ -183,16 +184,17 @@ def buildBowtie2idx(fs, out, name=None):
                     newF = NamedTemporaryFile()
                     SeqIO.write(s, newF.name, 'fasta')
                     newFs.append(newF.name)
-                    converted.append(newF) # for closing these files later
+                    converted.append(newF)  # for closing these files later
         except BaseException as err:
-            logging.error(f'Unexpected error {err=}, {type(err)=}') # python >= 3.8
+            # python >= 3.8
+            logging.error(f'Unexpected error {err=}, {type(err)=}')
         fs = newFs
 
-    logging.info('-'*20 + 'Indexing genome ' + getTime() + '-'*20)
+    logging.info('-' * 20 + 'Indexing genome ' + getTime() + '-' * 20)
     args = [
         'bowtie2-build',
         ','.join(fs),
-        os.path.join(out,bt2_base),
+        os.path.join(out, bt2_base),
     ]
     logging.info(' '.join(args))
     result = run(args=args, capture_output=True)
@@ -201,10 +203,10 @@ def buildBowtie2idx(fs, out, name=None):
     if result.returncode != 0:
         logging.info('stderr: ' + result.stderr.decode())
         logging.info('stdout: ' + result.stdout.decode())
-        logging.info('-'*20 + 'Error Indexing genome' + getTime() + '-'*20)
+        logging.info('-' * 20 + 'Error Indexing genome' + getTime() + '-' * 20)
         raise Exception
-    logging.info('-'*20 + 'DONE Indexing genome' + getTime() + '-'*20)
-    logging.info('\n'*2)
+    logging.info('-' * 20 + 'DONE Indexing genome' + getTime() + '-' * 20)
+    logging.info('\n' * 2)
 
     return os.path.join(out, bt2_base)
 
@@ -212,21 +214,16 @@ def buildBowtie2idx(fs, out, name=None):
 def imputePesuffix(rawFiles, rawExt, peSfx=None):
     if not isinstance(peSfx, type(None)):
         return peSfx
-    assert len(rawFiles)%2 == 0
+    assert len(rawFiles) % 2 == 0
     assert len(rawFiles) > 0
-    fns = [f[:-len(rawExt)] for f in rawFiles]
-    fns.sort()
+    fns = sorted([f[:-len(rawExt)] for f in rawFiles])
     for i in range(1, 12):
         s = set(fn[-i:] for fn in fns)
         if len(s) == 2:
-            peSfx = list(s)
-            peSfx.sort()
+            peSfx = sorted(s)
             logging.info(f'Found pairend suffix {peSfx}')
             return peSfx
     raise ValueError(f'pair end suffix not found in {rawFiles}')
-
-
-
 
 
 def getTime():
@@ -234,10 +231,9 @@ def getTime():
 
 
 def diffTime(a):
-    d = abs(time.time()-a)
-    h = int(d//3600)
+    d = abs(time.time() - a)
+    h = int(d // 3600)
     return str(h).zfill(2) + time.strftime(':%M:%S', time.gmtime(d))
-
 
 
 if __name__ == "__main__":
